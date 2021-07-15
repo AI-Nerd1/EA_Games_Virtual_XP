903d2fafd175fc5f3433f1efa43de8b2294f6366
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-07-15 14:03:11 +0300

    Add files via upload
    Vaxman with python3

---
+++ vaxman.py
@@ -0,0 +1,179 @@
+# VAXMAN1 Modified
+
+from random import choice
+from turtle import *
+import pygame
+import random
+
+from freegames import floor, vector
+
+
+def kill(self):
+    for group in self.__g:
+        group.remove_internal(self)
+    self.__g.clear()
+
+
+state = {'score': 0}
+path = Turtle(visible=False)        # path cursor made invisible
+writer = Turtle(visible=False)      # score cursor made invisible
+aim = vector(5, 0)
+vaxman = vector(0, 80)           # vaxman's starting position
+ghosts = [
+    [vector(-180, 160), vector(5, 0)],      # ghost starting coordinates
+    [vector(-180, -160), vector(0, 5)],
+    [vector(100, 160), vector(0, 5)],
+    [vector(100, -160), vector(-5, 0)]
+]
+tiles = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+
+
+def square(x, y):
+    path.up()
+    path.goto(x, y)
+    path.down()
+    path.begin_fill()
+
+    for count in range(4):
+        path.forward(20)            # Box sizes
+        path.left(90)
+
+    path.end_fill()
+
+
+def offset(point):
+    x = (floor(point.x, 20) + 200) / 20
+    y = (180 - floor(point.y, 20)) / 20
+    index = int(x + y * 20)
+    return index
+
+
+def valid(point):
+    index = offset(point)
+
+    if tiles[index] == 0:
+        return False
+    index = offset(point + 19)
+
+    if tiles[index] == 0:
+        return False
+
+    return point.x % 20 == 0 or point.y % 20 == 0
+
+
+def world():
+    bgcolor('skyblue')
+    path.color('green')
+
+    for index in range(len(tiles)):
+        tile = tiles[index]
+
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)       # food positions
+                path.dot(2, 'white')            # food size and color
+
+
+def move():
+    writer.undo()
+    writer.write(state['score'])
+
+    clear()                           # Clears tracks
+
+    if valid(vaxman + aim):
+        vaxman.move(aim)
+
+    index = offset(vaxman)
+
+    if tiles[index] == 1:
+        tiles[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+    up()
+    goto(vaxman.x + 10, vaxman.y + 10)
+    dot(15, 'yellow')                           # Vaxman size and color
+
+    for point, course in ghosts:
+        if valid(point + course):
+            point.move(course)
+        else:
+            options = [
+                vector(5, 0),
+                vector(-5, 0),
+                vector(0, 5),
+                vector(0, -5),
+            ]
+            plan = choice(options)
+            course.x = plan.x
+            course.y = plan.y
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(10, 'red')                      # Ghost size and color
+
+    # update()
+
+    for point, course in ghosts:
+       if abs(vaxman - point) < 15:
+           down()
+           dot(10, 'green')
+           # goto(vaxman.x + 10, vaxman.y + 10)
+
+           # return        # add the killing of the ghost here
+    # for point, course in ghosts:
+    #     def update(self):
+    #         if abs(vaxman - point) < 20:
+    #             ghosts.remove()
+
+    ontimer(move, 100)                  # Game time, less is fast
+
+
+def change(x, y):
+    if valid(vaxman + vector(x, y)):
+        aim.x = x
+        aim.y = y
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)                   # Show build of world
+writer.goto(160, 160)            # Location of score board
+writer.color('black')            # score color
+writer.write(state['score'])
+listen()
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
+world()
+move()
+done()
